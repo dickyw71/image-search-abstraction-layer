cbaab9f7a141d085479852cc38ef716d26eacafe
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-09-11 17:31:56 +0100

    Added outline of Express server for app.

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 // Server side js
+
+import express from 'express';
+
+let app = express();
+
+// routes
+app.get("/api/imagesearch", function(request, response) {
+    // call image search API
+    // format results as array of json objects
+    // store search in recent search list
+
+});
+
+app.get("/api/latest/magesearch", function(request, response) {
+    // return recent search list
+
+});
+
+// listen for requests :)
+let listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+  });
