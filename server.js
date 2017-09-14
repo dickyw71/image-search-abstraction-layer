ef0e745c9827208ddd6e896391690f4fe4777568
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-09-14 17:30:00 +0100

    Added call to Google Custom Search for images

--- server.js
+++ server.js
@@ -2,14 +2,24 @@
 
 //import express from 'express';
 const express = require('express');
+const fetch = require('node-fetch');
+
 let app = express();
 
 
 // routes
-app.get("/api/imagesearch", function(request, response) {
+app.get("/api/imagesearch/:searchString", function(request, response) {
+
+    let urlParams = request.params;
+    console.log(urlParams);
+
     // call image search API
+    fetch("https://www.googleapis.com/customsearch/v1?key=AIzaSyD82uWISxvyGw_T-zRx6xTCLkXS_wBVzvo&cx=015311179725308767903:ghrfxowvp_0&q=" + urlParams.searchString + "&searchType=image")
+        .then(response => response.json())
+        .then(json => console.log(json));
     // format results as array of json objects
     // store search in recent search list
+    
 
 });
 
