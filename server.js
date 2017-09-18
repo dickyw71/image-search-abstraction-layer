80923dc7f19cdc5366c7d8675d4b4e4d76949419
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-09-18 17:30:11 +0100

    Added dotenv to access the google custom search API key and id.

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 // Server side js
 
 //import express from 'express';
+const dotenv = require('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
 
+
 let app = express();
+
 
 
 // routes
 app.get("/api/imagesearch/:searchString", function(request, response) {
 
     let urlParams = request.params;
-    console.log(urlParams);
+    console.log(urlParams, process.env.CSE_API_KEY);
 
     // call image search API
-    fetch("https://www.googleapis.com/customsearch/v1?key=AIzaSyD82uWISxvyGw_T-zRx6xTCLkXS_wBVzvo&cx=015311179725308767903:ghrfxowvp_0&q=" + urlParams.searchString + "&searchType=image")
+    fetch("https://www.googleapis.com/customsearch/v1?key=" + process.env.CSE_API_KEY + "&cx=" + process.env.CSE_ID +  "&q=" + urlParams.searchString + "&searchType=image")
         .then(response => response.json())
         .then(json => console.log(json));
     // format results as array of json objects
