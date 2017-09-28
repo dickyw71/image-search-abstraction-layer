7862400ce62d7127297a3be89cd53586ce9c18b5
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-09-28 17:07:09 +0100

    Moved files to sub folders.

---
+++ src/image-search.js
@@ -0,0 +1,29 @@
+var fetch = require('node-fetch');
+const dotenv = require('dotenv').config();
+
+module.exports = function findImages(urlParams) {
+    // call image search API
+    fetch("https://www.googleapis.com/customsearch/v1?key=" + process.env.CSE_API_KEY + "&cx=" + process.env.CSE_ID + "&q=" + urlParams.searchString + "&searchType=image")
+        .then(response => response.json())
+        .then(json => formatObject(json));
+}
+
+function formatObject(json) {
+    imageObArr = json.items.map(item => {
+        return (
+            { 
+                imageUrl: item.link, 
+                altText: item.title,
+                parentPage: item.image.contextLink,
+                description: item.snippet,
+                thumbnail: {
+                    thumbnailUrl: item.image.thumbnailLink,
+                    thunbnailHeight: item.image.thunbnailHeight,
+                    thumbnailWidth: item.image.thumbnailWidth
+                }
+            }
+        )
+    })
+    console.log(imageObArr);
+    return imageObArr;
+}
