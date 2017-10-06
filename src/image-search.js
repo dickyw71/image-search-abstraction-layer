54fc92da38b8a38eee8d6fbbeb213fdf6e954561
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-10-06 07:59:21 +0100

    Added validation for query parameter.

--- src/image-search.js
+++ src/image-search.js
@@ -2,15 +2,30 @@ var fetch = require('node-fetch');
 const dotenv = require('dotenv').config();
 
 module.exports = function findImages(urlParams, query) {
+    let offset = validate(query);
     // call image search API and return a Promise
     return new Promise((resolve, reject) => {  
-        let offset = Number.isInteger(parseInt(query.offset)) > 0 ? parseInt(query.offset)*10 : '1';
         fetch("https://www.googleapis.com/customsearch/v1?key=" 
             + process.env.CSE_API_KEY + "&cx=" + process.env.CSE_ID + "&q=" + urlParams.searchString + "&searchType=image" + "&start=" + offset)
             .then(response => response.json())
             .then(json => resolve(formatSearchResultsObjectArray(json)))
             .catch(ex => reject(ex));
     });
+}
+
+function validate(query) {
+    // validate the query parameter
+    let offset = 1;
+    if (query.offset) {
+        let offset = Number.isInteger(parseInt(query.offset)) ? parseInt(query.offset) : '1';
+        if (offset > 1 && offset < 10) {
+            offset = offset * 10;
+        }
+        else {
+            offset = 1;
+        }
+    }
+    return offset;
 }
 
 // format search results as array of json objects
